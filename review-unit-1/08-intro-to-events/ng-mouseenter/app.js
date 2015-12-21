098f9ee2d5050568156f266657cf17e8fe32c225
Aubrey Wullschleger <aubrey.wullschleger@gmail.com>
2015-12-20 20:58:27 -0800

    1.7 && 1.8

---
+++ review-unit-1/08-intro-to-events/ng-mouseenter/app.js
@@ -0,0 +1,15 @@
+var app = angular.module('myApp', []);
+
+app.controller('MyController', ['$scope', function($scope) {
+    $scope.number = 5;
+
+    $scope.pickRandomNumber = function() {
+        $scope.number = Math.floor(Math.random() * 10) + 1
+    };
+
+    $scope.reverseWord = function() {
+        $scope.reversedWord = $scope.word.split('').reverse().join('');
+    };
+
+    $scope.enterCount = 0;
+}]);
